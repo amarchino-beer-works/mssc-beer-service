5642a1ea880c9affaa48e4263c4f49afaeb2200e
amarchino <marchino.alessandro@gmail.com>
2021-12-08 21:06:23 +0100

    Lesson 194

--- src/main/java/guru/springframework/msscbeerservice/services/brewing/BrewBeerListener.java
+++ src/main/java/guru/springframework/msscbeerservice/services/brewing/BrewBeerListener.java
@@ -3,6 +3,7 @@ package guru.springframework.msscbeerservice.services.brewing;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import guru.springframework.msscbeerservice.config.JmsConfig;
 import guru.springframework.msscbeerservice.domain.Beer;
@@ -20,6 +21,7 @@ public class BrewBeerListener {
 	private final BeerRepository beerRepository;
 	private final JmsTemplate jmsTemplate;
 
+	@Transactional
 	@JmsListener(destination = JmsConfig.BREWING_REQUEST_QUEUE)
 	public void listen(BrewBeerEvent event) {
 		BeerDto beerDto = event.getBeerDto();
